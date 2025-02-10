84f49b945e22f4f959f956ac684dd23e613fd92b
Godita <106154877+L0L4-chan@users.noreply.github.com>
2025-02-10 12:28:20 +0100

    Dialogos json

--- 2D/main.py
+++ 2D/main.py
@@ -12,3 +12,14 @@ Version: 1.0.0
 import pygame, sys , random
 
 pygame.init()
+
+#Const 
+WIDTH =  1020
+HEIGTH = 1020
+
+PAUSE = False
+#Will be initiate when the language is selected
+DIALOG = []
+
+
+
