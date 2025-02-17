c293ed7e7b665f0ab92349ed9533d66af6e5cac3
Godita <106154877+L0L4-chan@users.noreply.github.com>
2025-02-17 12:39:46 +0100

    Update dialogBox.py

--- 2D/src/views/dialogBox.py
+++ 2D/src/views/dialogBox.py
@@ -8,7 +8,8 @@ class DialogBox:
         self.gameManager = GameManager.get_instance()
         self.config = ConfigManager().get_instance()
         self.dialog = next((d for d in self.config.get_text(key="dialogues") if d["event"] == event), None)
-        self.icon = pygame.image.load(f"../Art/{self.config.get_artpath()}/avatar/{self.dialog["character"]}.png") # Carga el icono del personaje
+        name = self.dialog["character"]
+        self.icon = pygame.image.load(f"../Art/{self.config.get_artpath()}/avatar/{name}.png") # Carga el icono del personaje
         if bg_image: # Carga el fondo del cuadro de diálogo
             self.bg_image = pygame.image.load(bg_image)
         else: # Carga el fondo negro
